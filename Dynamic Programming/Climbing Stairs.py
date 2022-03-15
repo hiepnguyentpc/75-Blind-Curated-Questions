8eab099e47071c5c7ef093b54837b646db537b69
Hiep Nguyen <hiepnguyentpc@gmail.com>
2022-03-14 22:41:14 -0500

    Solved 3 Problems in DP

---
+++ Dynamic Programming/Climbing Stairs.py
@@ -0,0 +1,16 @@
+def climbStairs(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    if n == 1:
+        return 1
+    dp = []
+    dp = [0 for i in range(n)]
+    dp[0] = 1
+    dp[1] = 2
+
+    for i in range(2, n):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n - 1]
